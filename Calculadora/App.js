4fc299a6bcd00c9f51921650efaaabf570693f78
alessandraboeck <alessandraboeck72@gmail.com>
2024-04-29 19:52:46 -0300

    Correção nos espaçamentos
    Falta e excesso de espaçamentos.

--- Calculadora/App.js
+++ Calculadora/App.js
@@ -9,43 +9,42 @@ export default function App() {
   const [currentNumber, setCurrentNumber] = useState('');
   const [lastNumber, setLastNumber] = useState('');
 
-
-  function calculator(){
+  function calculator() {
     const splitNumbers = currentNumber.split(' ');
     const fistNumber = parseFloat(splitNumbers[0]);
     const lastNumber = parseFloat(splitNumbers[2]);
     const operator = splitNumbers[1];
 
     // Faz ação referente tecla pressionada
-    switch(operator){
+    switch (operator) {
       case '+':
         setCurrentNumber((fistNumber + lastNumber).toString());
         return;
-      case '-': 
+      case '-':
         setCurrentNumber((fistNumber - lastNumber).toString());
         return;
       case 'x':
         setCurrentNumber((fistNumber + lastNumber).toString());
         return;
-      case '/': 
+      case '/':
         setCurrentNumber((fistNumber - lastNumber).toString());
         return;
     }
   }
 
-  function handleInput(buttonPressed){
-    console.log(buttonPressed);// Mostra no Console a tecla pressionada
+  function handleInput(buttonPressed) {
+    console.log(buttonPressed); // Mostra no Console a tecla pressionada
     if(buttonPressed === '+' | buttonPressed === '-' | buttonPressed === 'x' | buttonPressed === '/' ){
-      setCurrentNumber(currentNumber + '' + buttonPressed + '')
-      return
+      setCurrentNumber(currentNumber + '' + buttonPressed + '');
+      return;
     }
-    switch(buttonPressed){
+    switch (buttonPressed) {
       case 'DEL':
         setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 2)))
         return;
       case 'LIMPAR': // Limpa todo o conteúdo
-        setLastNumber('') ;
-        setCurrentNumber('') ;
+        setLastNumber('');
+        setCurrentNumber('');
         return;
       case '=':
         setLastNumber(currentNumber + '=');
@@ -58,10 +57,8 @@ export default function App() {
     setCurrentNumber(currentNumber + buttonPressed);
   }
 
-
   return (
     <View style={styles.container}>
-
       {/* Area onde o resultado é exibido */}
       <View style={styles.results}>
         <Text style={styles.historyText}>{lastNumber}</Text>
@@ -70,13 +67,12 @@ export default function App() {
 
       {/* Area onde os botões são exibidos*/}
       <View style={styles.buttons}>
-
         {buttons.map((button) => 
-          button === '=' ? // Mapeamento do botão =
+          button === '=' ?// Mapeamento do botão =
         <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
-          : // Mapeamento dos outros botões
+          :// Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
             <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
           </TouchableOpacity>
@@ -103,7 +99,7 @@ const styles = StyleSheet.create({
     padding: 12,
     textAlign: 'right',
   },
-  historyText:{
+  historyText: {
     color: '#7c7c7c',
     fontSize: 20,
     marginRight: 10,
@@ -117,12 +113,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    minWidth: 90, 
+    minWidth: 90,
     minHeight: 90,
     flex: 2,
   },
   textButton: {
     color: '#7c7c7c',
     fontSize: 20,
-  } ,
+  },
 });
